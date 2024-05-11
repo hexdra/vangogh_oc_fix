2334813f06d831b31791f1928155e2e0570a8e5d
hexdra <42896910+hexdra@users.noreply.github.com>
2024-05-11 05:58:05 +0200

    Update get.sh
    fix for steamos 3.6, maybe breaks other versions

--- linux-header-extract/get.sh
+++ linux-header-extract/get.sh
@@ -2,9 +2,8 @@ if ls ./linux-pkg
     then rm -rf ./linux-pkg
 fi
 UNAME=$(uname -r)
-VERSION=$(echo $UNAME |awk -F"-" '{print $NF}')
-CUT=${UNAME/-/.}
-CUT=${CUT/-neptune-$VERSION/}
+VERSION=$(echo $UNAME | awk -F '-' '{print $5}')
+CUT=$(echo $UNAME | awk -F '-' '{print $1"."$2"."$3}' | rev | sed 's/\./-/' | rev)
 curl -L https://steamdeck-packages.steamos.cloud/archlinux-mirror/sources/jupiter-main/linux-neptune-${VERSION}-${CUT}.src.tar.gz -o linux-pkg.tar.gz
 tar -xvf linux-pkg.tar.gz
 mv ./linux-neptune-$VERSION ./linux-pkg
